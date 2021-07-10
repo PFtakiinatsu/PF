b98d2d18ce8b154c8bedb45c6c0f5ce418da572c
coconut72t <natsu.coco3108@gmail.com>
2021-07-10 07:48:48 +0000

    基本機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    question = Question.find(params[:question_id])
+    comment = current_user.comments.new(comment_params)
+    comment.question_id = question.id
+    comment.save
+    redirect_to question_path(question)
+  end
+
+  def destroy
+    Comment.find_by(id: params[:id], question_id: params[:question_id]).destroy
+    redirect_to question_path(params[:question_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
