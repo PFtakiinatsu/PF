fd1a204289fce76a2678258d4c28b0ae85d09c38
coconut72t <natsu.coco3108@gmail.com>
2021-07-29 17:08:01 +0000

    削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :question_id)
   end
 end
