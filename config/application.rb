16322578ffce3aabdb4cc561acd79a2d4facc9b4
coconut72t <natsu.coco3108@gmail.com>
2021-07-29 04:58:32 +0000

    質問詳細

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module PF
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja #追加
+    config.time_zone = "Tokyo" #タイムゾーンを東京に
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_075044) do
+ActiveRecord::Schema.define(version: 2021_07_29_040638) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_075044) do
     t.string "image_id"
     t.string "title"
     t.text "body"
-    t.integer "point"
+    t.integer "point", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "best_answer_id"
