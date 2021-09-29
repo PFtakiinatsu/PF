fa3803785764e3b716a3e9aed506ec085f260604
coconut72t <natsu.coco3108@gmail.com>
2021-09-29 14:59:13 +0000

    questions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# 同じ内容の文字列リテラルは同一の文字列オプジェクトを返すようになる
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
